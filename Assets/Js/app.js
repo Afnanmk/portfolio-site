20528cc64178dbf50a6edf92d6e79daddbe1b31f
Afnan Malik <Afnanmalik80@gmail.com>
2022-01-13 21:41:19 +0300

    shapes added in hero section

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,8 +1,12 @@
 // SHOW NAVBAR ON SCROLL
 const header = document.querySelector("header")
 const sectionOne = document.querySelector(".hero")
+const BACK_TO_TOP = document.querySelector(".back_to_top")
+const navLink = document.querySelectorAll(".nav__link")
 
-const sectionOneOptions = {}
+const sectionOneOptions = {
+  threshold: 0.3,
+}
 
 const sectionOneObserver = new IntersectionObserver(function (
   entries,
@@ -11,11 +15,22 @@ const sectionOneObserver = new IntersectionObserver(function (
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       header.classList.add("sticky-navbar")
+      BACK_TO_TOP.classList.add("back_to_top--intersecting")
     } else {
       header.classList.remove("sticky-navbar")
+      BACK_TO_TOP.classList.remove("back_to_top--intersecting")
     }
   })
 },
 sectionOneOptions)
 
 sectionOneObserver.observe(sectionOne)
+
+// Code for changing active link on click
+navLink.forEach((link) => {
+  link.addEventListener("click", function () {
+    const activeNav = document.querySelector(".active-nav")
+    activeNav.className = activeNav.className.replace(" active-nav", "")
+    this.className += " active-nav"
+  })
+})
